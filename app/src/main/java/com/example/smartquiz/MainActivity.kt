f22786ede9b24be7680bbc5dfac2304ea5109799
Hamburgir <shantnuprofid@gmail.com>
2026-01-25 01:58:29 +0530

    Added database smoke test to MainActivity
    - Injected `AppDatabase` using Hilt.
    - Added a test coroutine to insert a dummy user and log the database count.
    - Added logging to verify database instance creation and data persistence.

--- app/src/main/java/com/example/smartquiz/MainActivity.kt
+++ app/src/main/java/com/example/smartquiz/MainActivity.kt
@@ -4,16 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
+import com.example.smartquiz.data.local.database.AppDatabase
+import com.example.smartquiz.data.local.entity.user.UserEntity
 import com.example.smartquiz.navigation.RootNavGraph
 import com.example.smartquiz.ui.theme.SmartQuizTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject lateinit var db: AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        android.util.Log.d("DB_CHECK", "DB instance created: $db")
+        lifecycleScope.launch {
+            val userDao = db.userDao()
+
+            userDao.insert(
+                UserEntity(
+                    userId = "test_1",
+                    name = "Test User",
+                    email = "test@gmail.com",
+                    photoUrl = null
+                )
+            )
+
+            val users = userDao.getAll()
+            android.util.Log.d("DB_DATA", "Users count = ${users.size}")
+        }
+
         setContent {
             SmartQuizTheme {
                 val navController = rememberNavController()
